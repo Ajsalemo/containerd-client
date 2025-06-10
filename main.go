0a9ac53d434e8d04fc15c514ca98f8e63e3f4a8e
Anthony Salemo <ajssalemo@gmail.com>
2025-06-09 22:28:17 -0400

    experimentinig with decoupled containerd task clients and using flags pkg  for parsing

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
-
 	"strconv"
 
+	"github.com/containerd/containerd/api/services/tasks/v1"
 	"github.com/containerd/containerd/namespaces"
 	containerd "github.com/containerd/containerd/v2/client"
 	"github.com/containerd/containerd/v2/pkg/cio"
@@ -19,105 +20,94 @@ func init() {
 }
 
 func main() {
-	// Connect to containerd
-	zap.L().Info("Establishing connection to containerd via /run/containerd.sock...")
-	if err := connect(); err != nil {
-		zap.L().Fatal(err.Error())
-	}
-}
+	// Parse command line flags
+	stop := flag.Bool("stop", false, "Stop the running task")
+	delete := flag.Bool("delete", false, "Delete the task after stopping it")
+	taskId := flag.String("task", "", "Task ID to stop")
+	run := flag.Bool("run", false, "Run the task after creating it")
+	flag.Parse()
+	ctx := namespaces.WithNamespace(context.Background(), "default")
 
-func connect() error {
 	client, err := containerd.New("/run/containerd/containerd.sock")
 	if err != nil {
-		return err
+		return
 	}
-	defer client.Close()
-	ctx := namespaces.WithNamespace(context.Background(), "default")
-	containerdVersion, err := client.Version(context.Background())
-	if err != nil {
-		return err
+	// Run - this executes most lifecycle events for container and task creation
+	if *run {
+		defer client.Close()
+		containerdVersion, err := client.Version(context.Background())
+		if err != nil {
+			return
+		}
+		zap.L().Info("Connected to containerd", zap.String("version", containerdVersion.Version), zap.String("revision", containerdVersion.Revision), zap.String("socket", "/run/containerd/containerd.sock"))
+		// Pull an image
+		zap.L().Info("Pulling image..")
+		image, err := client.Pull(ctx, "docker.io/library/redis:alpine", containerd.WithPullUnpack)
+		if err != nil {
+			return
+		}
+
+		imageSize, err := image.Size(ctx)
+		if err != nil {
+			return
+		}
+		zap.L().Info("Pulled image", zap.String("name", image.Name()), zap.String("digest", image.Target().Digest.String()), zap.String("size", strconv.FormatInt(imageSize, 10)), zap.String("mediaType", image.Target().MediaType))
+		// Create a container
+		u := uuid.New()
+		containerName := fmt.Sprintf("container-%s", u.String())
+		zap.L().Info("Creating container " + containerName + " with snapshot " + fmt.Sprintf("snapshot-%s", u.String()))
+		container, err := client.NewContainer(
+			ctx,
+			containerName,
+			containerd.WithNewSnapshot(fmt.Sprintf("snapshot-%s", u.String()), image),
+			containerd.WithNewSpec(oci.WithImageConfig(image)),
+		)
+		if err != nil {
+			return
+		}
+		defer container.Delete(ctx, containerd.WithSnapshotCleanup)
+
+		zap.L().Info("Created container " + containerName + " with container ID " + container.ID())
+		// Create a task from the container
+		zap.L().Info("Creating task for container", zap.String("containerID", container.ID()))
+		task, err := container.NewTask(ctx, cio.NewCreator(cio.WithStdio))
+		if err != nil {
+			return
+		}
+		defer task.Delete(ctx)
+		zap.L().Info("Created task for container", zap.String("containerID", container.ID()), zap.String("taskID", task.ID()))
+		// Run the task
+		zap.L().Info("Task started", zap.String("taskID", task.ID()))
+		// First variable is the exit status
+		task.Wait(ctx)
+		// call start on the task to execute the redis server
+		if err := task.Start(ctx); err != nil {
+			return
+		}
+		zap.L().Info("Task started", zap.String("taskID", task.ID()))
 	}
-	zap.L().Info("Connected to containerd", zap.String("version", containerdVersion.Version), zap.String("revision", containerdVersion.Revision), zap.String("socket", "/run/containerd/containerd.sock"))
-	// Pull an image
-	if err := pullImage(client, ctx); err != nil {
-		zap.L().Error("Failed to pull image", zap.Error(err))
+	// Stop a task.  This does NOT delete it. This just puts it into a "Stopped" state
+	if *stop && *taskId != "" {
+		zap.L().Info("Stopping task", zap.String("taskId", *taskId))
+
+		clientTask := client.TaskService()
+		_, err := clientTask.Kill(ctx, &tasks.KillRequest{ContainerID: string(*taskId), Signal: 15})
+		if err != nil {
+			zap.L().Error("Failed to kill task", zap.Error(err))
+			return
+		}
+		zap.L().Info("Killed task", zap.String("taskId", *taskId))
 	}
-	return nil
-}
+	// Delete a task (after stopping it)
+	if *delete && *taskId != "" {
+		zap.L().Info("Deleting task", zap.String("taskId", *taskId))
 
-func pullImage(client *containerd.Client, ctx context.Context) error {
-	zap.L().Info("Pulling image..")
-	image, err := client.Pull(ctx, "docker.io/library/redis:alpine", containerd.WithPullUnpack)
-	if err != nil {
-		return err
+		clientTask := client.TaskService()
+		_, err := clientTask.Delete(ctx, &tasks.DeleteTaskRequest{ContainerID: string(*taskId)})
+		if err != nil {
+			zap.L().Error("Failed to delete task", zap.Error(err))
+			return
+		}
+		zap.L().Info("Deleted task", zap.String("taskId", *taskId))
 	}
-
-	imageSize, err := image.Size(ctx)
-	if err != nil {
-		return err
-	}
-	zap.L().Info("Pulled image", zap.String("name", image.Name()), zap.String("digest", image.Target().Digest.String()), zap.String("size", strconv.FormatInt(imageSize, 10)), zap.String("mediaType", image.Target().MediaType))
-	// Create a container from the image
-	if err := createContainer(client, image, ctx); err != nil {
-		zap.L().Error("Failed to create container", zap.Error(err))
-	}
-
-	return nil
-}
-
-func createContainer(client *containerd.Client, image containerd.Image, ctx context.Context) error {
-	u := uuid.New()
-	containerName := fmt.Sprintf("container-%s", u.String())
-	zap.L().Info("Creating container " + containerName + " with snapshot " + fmt.Sprintf("snapshot-%s", u.String()))
-	container, err := client.NewContainer(
-		ctx,
-		containerName,
-		containerd.WithNewSnapshot(fmt.Sprintf("snapshot-%s", u.String()), image),
-		containerd.WithNewSpec(oci.WithImageConfig(image)),
-	)
-	if err != nil {
-		return err
-	}
-	defer container.Delete(ctx, containerd.WithSnapshotCleanup)
-
-	zap.L().Info("Created container " + containerName + " with container ID " + container.ID())
-	// Create a task for the container
-	if err := createTask(container, ctx); err != nil {
-		zap.L().Error("Failed to create task for container", zap.Error(err))
-	}
-
-	return nil
-}
-
-func createTask(container containerd.Container, ctx context.Context) error {
-	zap.L().Info("Creating task for container", zap.String("containerID", container.ID()))
-	task, err := container.NewTask(ctx, cio.NewCreator(cio.WithStdio))
-	if err != nil {
-		return err
-	}
-	defer task.Delete(ctx)
-	zap.L().Info("Created task for container", zap.String("containerID", container.ID()), zap.String("taskID", task.ID()))
-
-	// Run the task
-	if err := runTask(task, ctx); err != nil {
-		zap.L().Error("Failed to run task", zap.Error(err))
-	}
-
-	return nil
-}
-
-func runTask(task containerd.Task, ctx context.Context) error {
-	zap.L().Info("Task started", zap.String("taskID", task.ID()))
-	// First variable is the exit status
-	_, err := task.Wait(ctx)
-	if err != nil {
-		return err
-	}
-
-	zap.L().Info("Starting task", zap.String("taskID", task.ID()))
-	if err := task.Start(ctx); err != nil {
-		return err
-	}
-
-	return nil
 }
